82f948caf25fb5adf89e09f51ddef564fd27ced8
MartinPage1 <81260515+MartinPage1@users.noreply.github.com>
2022-01-31 22:53:01 -0600

    Images Fix

--- Unit1/images/sketch.js
+++ Unit1/images/sketch.js
@@ -1,15 +1,17 @@
-  let axe, crossbow, shield;
+let axe;
+let crossbow;
+let shield;
 
 function setup() {
-  createCanvas(500, 500);
-  axe = loadImage(“assets/Axe.png”) ;
-  crossbow = loadImage(“assets/Crossbow.png”) ;
-  shield = loadImage(“assets/Shield.png”) ;
+  createCanvas(1000, 1000);
 
+  axe = loadImage('assets/axe.png') ;
+  crossbow = loadImage('assets/crossbow.png') ;
+  shield = loadImage('assets/shield.png') ;
 }
 
 function draw() {
-  image(axe, width / 2, 100, 400, 400) ;
-  image(crossbow, width / 2, 200, 400, 400) ;
-  image(shield, width / 2, 300, 400, 400) ;
+  image(crossbow, 200, 200, 400, 400) ;
+  image(axe, 100, 100, 400, 400) ;
+  image(shield, 400, 200, 200, 200) ;
 }
