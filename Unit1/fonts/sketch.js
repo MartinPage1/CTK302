467fc142f0a4ec411d24fede1ae0284a14fe06ba
MartinPage1 <81260515+MartinPage1@users.noreply.github.com>
2022-01-31 23:10:11 -0600

    FontsColro

--- Unit1/fonts/sketch.js
+++ Unit1/fonts/sketch.js
@@ -12,10 +12,11 @@ function setup() {
 function draw() {
   background (100);
   textSize(80);
-
+  fill('White');
   textFont(font1);
   text("Hello.", width/2, 100);
 
+  fill('red');
   textFont(font2);
   text("How are ya?", width/2, 200);
 
