2b27656853d9c8f2b898b78e6c122e9351a0e7d0
MartinPage1 <81260515+MartinPage1@users.noreply.github.com>
2022-03-31 10:43:16 -0500

    Fix

--- Unit3/mp3/sketch.js
+++ Unit3/mp3/sketch.js
@@ -96,7 +96,7 @@ function draw() {
       for (let i = 0; i < maxcash+50; i++) {
         cash.push(new Cash());
       }
-      state = 4;
+      state = 5;
       break;
     case 4: // win!
       background("darkblue");
